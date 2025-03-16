da76641fdb84c67fba2a19dd89c8aaf456c9b1fc
Davida <byourself77by@gmail.com>
2025-03-16 22:46:33 +0000

    Fix Error

--- routes/userrouter.mjs
+++ routes/userrouter.mjs
@@ -1,6 +1,6 @@
 //External Package imports
 import  {Router} from 'express';
-
+import {profilePic} from "../middlewares/upload.mjs";
 //Internal variables imports
 import {
     userSignup,
@@ -19,7 +19,7 @@ userRouter.post('/signup', userSignup); //Test Successful
 userRouter.post('/login', userSignIn); //Test Successful
 userRouter.post('/logout', userLogOut); //Test Successful
 userRouter.get('/getUser', verifyToken, getUser);//Test Successful
-userRouter.patch('/updateUser', verifyToken, updateProfile); //Test Successful
+userRouter.patch('/updateUser', verifyToken, profilePic.single('profilePicture'), updateProfile); //Test Successful
 userRouter.delete('/deleteUser', verifyToken, deleteProfile); //Test Successful
 
 
